60cdc5cca7485804d51f20fed1aa0477f9507658
albasanchez00 <albasanchezolmedo00@gmail.com>
2024-10-18 13:58:30 +0200

    Actu 3

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
-
-if (validarCpostal($_REQUEST["cp"])
-    && validarDni($_REQUEST["dni"])
-    && validarEdad($_REQUEST["fNacimiento"])
-    && validarTexto($_REQUEST["nombre"])
-    && validarTexto($_REQUEST["pApellido"])
-    && validarTelefono($_REQUEST["telefono"])
-    && !empty($_REQUEST["provincia"])
-    && !empty($_REQUEST["uEstudio"])
-    && !empty($_REQUEST["fechaUltEstudio"])
-    && !empty($_REQUEST["direccion"])) {
-    header("Location: ../vista/formulario2.php");
-} else {
-    header("Location: ../vista/formulario1.php?errores=Faltan datos");
+if ($_REQUEST["origen"]==="formulario1"){
+    if (validarCpostal($_REQUEST["cp"])
+        && validarDni($_REQUEST["dni"])
+        && validarEdad($_REQUEST["fNacimiento"])
+        && validarTexto($_REQUEST["nombre"])
+        && validarTexto($_REQUEST["pApellido"])
+        && validarTelefono($_REQUEST["telefono"])
+        && !empty($_REQUEST["provincia"])
+        && !empty($_REQUEST["uEstudio"])
+        && !empty($_REQUEST["fechaUltEstudio"])
+        && !empty($_REQUEST["direccion"])) {
+        header("Location: ../vista/formulario2.php");
+    } else {
+        header("Location: ../vista/formulario1.php?errores=Faltan datos");
+    }
 }
 function validarTexto($texto)
 {
