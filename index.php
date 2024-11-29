648e53be6fd576ab7c4b27f810d1c7c55387bd56
albasanchez00 <albasanchezolmedo00@gmail.com>
2024-11-29 07:49:17 +0100

    Actu style

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CEPA</title>
-    <link rel="stylesheet" href="vista/css/style.css">
-</head>
-<body>
+<?php
+include_once("vista/header.php");
+?>
+
     <h1>Bienvenido</h1>
     <section class="button-containers">
         <div class="boton">
@@ -22,5 +15,6 @@
         </div>
     </section>
 
-</body>
-</html>
+<?php
+include_once("vista/footer.php");
+?>
